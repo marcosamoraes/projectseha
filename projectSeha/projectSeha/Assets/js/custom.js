e004f2cf97e7429e588e6f1748abc7fd4d7a055a
Marcos Moraes <marcos.amoraes@outlook.com>
2016-08-24 22:40:15 -0300

    View Availability

--- projectSeha/projectSeha/Assets/js/custom.js
+++ projectSeha/projectSeha/Assets/js/custom.js
@@ -1,1 +1,36 @@
 // Aqui colocamos nosso próprio js
+
+$(document).ready(function(){
+
+	var color = 'transparent';
+
+	//Select colors (Slots)
+	$('#eraser').click(function(){
+		color = 'transparent';
+
+		$(this).css('opacity', '1');
+		$('#available').css('opacity', '0.5');
+		$('#maybe').css('opacity', '0.5');
+	});
+
+	$('#available').click(function(){
+		color = '#7cfc00';
+	
+		$(this).css('opacity', '1');
+		$('#eraser').css('opacity', '0.5');
+		$('#maybe').css('opacity', '0.5');
+	});
+
+	$('#maybe').click(function(){
+		color = '#ffbf00';
+
+		$(this).css('opacity', '1');
+		$('#eraser').css('opacity', '0.5');
+		$('#available').css('opacity', '0.5');
+	});
+
+	$('.slots-content').click(function(){
+		$(this).css('background-color', color);
+	});
+
+});
