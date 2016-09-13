0ddbba43562027fb092ef6fd18f41789cdeb8c74
Thelio <Thelio@THELIO>
2016-09-12 23:08:27 -0300

    problema com scrollbar na view steps

--- projectSeha/projectSeha/Assets/js/custom.js
+++ projectSeha/projectSeha/Assets/js/custom.js
@@ -112,7 +112,7 @@ $(document).ready(function () {
     });
 
     $("#txt-step1-prof").keyup(function () {
-        $('#select-step-status').val("ALL");
+        $('#select-step1-status').val("");
         var colunaprof = '#tb-step1 td:nth-child(1)';
         var valor = $(this).val().toUpperCase();
         $("#tb-step1 tbody tr").show();
@@ -122,7 +122,7 @@ $(document).ready(function () {
             }
         });
     });
-    $("#select-step-status").click(function () {
+    $("#select-step1-status").click(function () {
         $('#txt-step1-prof').val("");
         var colunastatus = '#tb-step1 td:nth-child(2)';
         var valor = $(this).val().toUpperCase();
@@ -135,5 +135,37 @@ $(document).ready(function () {
             });
         }   
     });
+    /*Reaproveitar código do filtro da step1*/
+    $("#txt-step2-prof").keyup(function () { 
+        $('#select-step2-status').val("");
+        var colunaprof = '#tb-step2 td:nth-child(1)';
+        var valor = $(this).val().toUpperCase();
+        $("#tb-step2 tbody tr").show();
+        $(colunaprof).each(function () {
+            if ($(this).text().toUpperCase().indexOf(valor) < 0) {
+                $(this).parent().hide();
+            }
+        });
+    });
+    /*Reaproveitar código do filtro da step1*/
+    $("#select-step2-status").click(function () {
+        $('#txt-step2-prof').val("");
+        var colunastatus = '#tb-step2 td:nth-child(2)';
+        var valor = $(this).val().toUpperCase();
+        $("#tb-step2 tbody tr").show();
+        if (valor != "ALL") {
+            $(colunastatus).each(function () {
+                if ($(this).text().toUpperCase().indexOf(valor) < 0) {
+                    $(this).parent().hide();
+                }
+            });
+        }
+    });
+    
+    $("#tb-step2 tr td").click(function () {
+        $("#p-disp-step2").html(""+$(this).text());
+    });
 
+
+    
 });
