5d816e6defcd3dae160b436381e28be1ae6e0384
Marcos Moraes <marcos.amoraes@outlook.com>
2016-09-18 23:32:08 -0300

    create controller e view profile + correções link logo

---
+++ projectSeha/projectSeha/Controllers/profileController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectSeha.Controllers
+{
+    public class profileController : Controller
+    {
+        // GET: profile
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
