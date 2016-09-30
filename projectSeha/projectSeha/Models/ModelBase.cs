7cc938c635a91f4961d27e0ddfe627cbbb5cd29a
daviagustos <davi.santos14@fatec.sp.gov.br>
2016-09-30 19:37:24 -0300

    Inicio da criação da modelBase.

---
+++ projectSeha/projectSeha/Models/ModelBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace ProjectSeha.Models
+{
+    public abstract class ModelBase : IDisposable
+    {
+        protected SqlConnection connection = null;
+
+        public ModelBase()
+        {
+            string connectionString = @"Data Source:localhost;
+                                        Initial Catalog=BDSeha; 
+                                        Integrated Security=true";
+
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+        }
+
+        public void Dispose()
+        {
+            connection.Close();
+        }
+    }
+}
