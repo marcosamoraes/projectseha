6ec24233061b83bea766321660c6f9ed2719fc98
Thelio <Thelio@THELIO>
2016-10-15 15:50:13 -0300

    Model professor e pessoa em progresso

---
+++ projectSeha/projectSeha/Models/PessoaModel.cs
@@ -0,0 +1,34 @@
+﻿using ProjectSeha.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ProjectSeha.Models
+{
+    public class PessoaModel : ModelBase
+    {
+        public Pessoa Login(string email, string senha)
+        {
+            Usuario e = null;
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = @"SELECT * FROM Usuario WHERE Email = @email and Senha = @senha";
+
+            cmd.Parameters.AddWithValue("@email", email);
+            cmd.Parameters.AddWithValue("@senha", senha);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                e = new Usuario();
+                e.IdUsuario = (int)reader["IdUsuario"];
+                e.Nome = (string)reader["Nome"];
+                e.Email = (string)reader["Email"];
+            }
+            return e; //se n entrar no if vai retornar usuario null
+        }
+    }
+}
---
+++ projectSeha/projectSeha/Models/ProfessorModel.cs
@@ -0,0 +1,80 @@
+﻿using ProjectSeha.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ProjectSeha.Models
+{
+    public class ProfessorModel : ModelBase
+    {
+        public List<Professor> Read()
+        {
+            List<Professor> lista = new List<Professor>();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = "SELECT * FROM ViewProfessores";
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Professor e = new Professor();
+               
+                e.CodPessoa = (int)reader["IdPessoa"];
+                e.Nome = (string)reader["Nome"];
+                e.Email = (string)reader["Email"];
+                e.Senha = (string)reader["Senha"];
+                e.Permissao_admin = (bool)reader["Permissao_admin"];
+                e.NomeGuerra = (string)reader["NomeGuerra"];
+                e.HorasAula = (int)reader["HorasAula"];
+                e.ExisteProfessor = (bool)reader["ExisteProfessor"];
+                e.InativaProfessor = (bool)reader["InativaProfessor"];
+                e.Observacoes = (string)reader["Observacoes"];
+
+                lista.Add(e);
+            }
+            return lista;
+        }
+
+        public void Create(Usuario e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = @"INSERT INTO Usuario VALUES (@nome, @email, @senha)";
+
+            cmd.Parameters.AddWithValue("@nome", e.Nome);
+            cmd.Parameters.AddWithValue("@email", e.Email);
+            cmd.Parameters.AddWithValue("@senha", e.Senha);
+
+            cmd.ExecuteNonQuery();
+        }
+
+        public void Update(Usuario e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = @"UPDATE Usuario SET Nome = @nome, Email = @email WHERE IdUsuario = @idUsuario";
+
+            cmd.Parameters.AddWithValue("@nome", e.Nome);
+            cmd.Parameters.AddWithValue("@email", e.Email);
+            cmd.Parameters.AddWithValue("@idUsuario", e.IdUsuario);
+
+            cmd.ExecuteNonQuery();
+        }
+
+
+        public void Delete(int id)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = @"DELETE FROM Usuario WHERE IdUsuario = @id";
+
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.ExecuteNonQuery();
+        }
+
+    }
+}
