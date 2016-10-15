5f426f873d783cbc66c8f3c6a6987c884b545b2a
Thelio <Thelio@THELIO>
2016-10-15 00:09:59 -0300

    Models restantes criados

---
+++ projectSeha/projectSeha/Entity/Resultado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectSeha.Entity
+{
+    public class Resultado
+    {
+        public int CodProfessor { get; set; }
+        public int CodDisciplina { get; set; }
+        public int CodSlot { get; set; }
+    }
+}
