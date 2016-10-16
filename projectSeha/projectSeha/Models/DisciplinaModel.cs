510e89356463b364fb2319d95d6747d636bf6cac
João Ignan <jao_ignan@hotmail.com>
2016-10-16 20:46:41 -0200

    Curso e Disciplina models inicializados

---
+++ projectSeha/projectSeha/Models/DisciplinaModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectSeha.Models
+{
+    public class DisciplinaModel : ModelBase
+    {
+    }
+}
