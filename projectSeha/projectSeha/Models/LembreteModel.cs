a1c4b16e3d4db814bb19895ad9066662518ea1ae
Marcos Moraes <marcos.amoraes@outlook.com>
2016-10-16 15:10:16 -0200

    create LembreteModel

--- projectSeha/projectSeha/Models/LembreteModel.cs
+++ projectSeha/projectSeha/Models/LembreteModel.cs
@@ -1,12 +1,68 @@
-﻿using System;
+﻿using ProjectSeha.Entity;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
 namespace ProjectSeha.Models
 {
-    public class LembreteModel
+    public class LembreteModel : ModelBase
     {
+        public List<Lembrete> Read()
+        {
+            List<Lembrete> lista = new List<Lembrete>();
 
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = "SELECT * FROM ViewLembrete";
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Lembrete e = new Lembrete();
+
+                e.LembreteId = (int)reader["LembreteId"];
+                e.Data = (DateTime)reader["Data"];
+                e.Conteudo = (string)reader["Conteudo"];
+
+                lista.Add(e);
+            }
+            return lista;
+        }
+
+        public void Create(Lembrete e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = @"EXEC ArmazenaLembrete @conteudo";
+
+            cmd.Parameters.AddWithValue("@conteudo", e.Conteudo);
+
+            cmd.ExecuteNonQuery();
+        }
+
+        public void Update(Lembrete e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = @"EXEC AlteraLembrete @conteudo";
+
+            cmd.Parameters.AddWithValue("@conteudo", e.Conteudo);
+
+            cmd.ExecuteNonQuery();
+        }
+
+
+        public void Delete(int id)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = @"ApagaLembrete @id";
+
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.ExecuteNonQuery();
+        }
     }
 }
