88a154e4290e09f629a508781381d221332debd1
Thelio <Thelio@THELIO>
2016-11-01 21:22:36 -0200

    lista de lembrete temporária

--- projectSeha/projectSeha/Controllers/admin/adminController.cs
+++ projectSeha/projectSeha/Controllers/admin/adminController.cs
@@ -15,15 +15,31 @@ namespace ProjectSeha.Controllers
         {
             using (LembreteModel model = new LembreteModel())
             {
-                List<Lembrete> listaLemb = model.Read();
+                //List<Lembrete> listaLemb = model.Read();
+                List<Lembrete> listaLemb = new List<Lembrete>();
+
+                Lembrete l1 = new Lembrete();
+                l1.LembreteId = 1;
+                l1.Data = "10/10/10";
+                l1.Conteudo = "Ablablabla";
+                listaLemb.Add(l1);
+
+                Lembrete l2 = new Lembrete();
+                l1.LembreteId = 2;
+                l1.Data = "11/11/11";
+                l1.Conteudo = "Clauclaulcau";
+                listaLemb.Add(l2);
+
                 ViewBag.ListLembrete = listaLemb;
                 return View();
             }
         }
+
         public ActionResult Steps()
         {
             return View();
         }
+
         public ActionResult Password()
         {
             return View();
