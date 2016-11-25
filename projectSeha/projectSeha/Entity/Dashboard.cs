fe96c851a4819f9bd7bae4ad0d0a1caf82865ce1
Thelio <marquesthelio@hotmail.com>
2016-11-24 22:34:05 -0200

    entity e model do dashboard ok!

---
+++ projectSeha/projectSeha/Entity/Dashboard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectSeha.Entity
+{
+    public class Dashboard
+    {
+        public class CountProf_Discp
+        {
+            public string Nome { get; set; }
+            public int QtdDisciplinas { get; set; }
+        }
+
+        public class CountProf_Curso
+        {
+            public string Nome { get; set; }
+            public int QtdCursos { get; set; }
+        }
+
+        public class CountCurso_Discp
+        {
+            public string Titulo { get; set; }
+            public string Turno { get; set; }
+            public int QtdDisciplinas { get; set; }
+        }
+    }
+}
---
+++ projectSeha/projectSeha/Models/DashboardModel.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using ProjectSeha.Entity;
+using System.Data;
+
+namespace ProjectSeha.Models
+{
+    public class DashboardModel : ModelBase
+    {
+        public int CountProf() //retorna total de professores cadastrados
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = "SELECT COUNT(*) Professores from tblProfessor";
+            try
+            {
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return (int)reader["Professores"];
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        public int CountDiscp() //retorna total de disciplinas cadastrados
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = "SELECT COUNT(*) Disciplinas from tblDisciplina";
+            try
+            {
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return (int)reader["Disciplinas"];
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        public int CountCurso() //retorna total de courses cadastrados
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = "SELECT COUNT(*) Cursos from tblCurso";
+            try
+            {
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return (int)reader["Cursos"];
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        public List<Dashboard.CountProf_Discp> CountProf_Discp() //retorna qtd de diciplinas por professor
+        {
+            List<Dashboard.CountProf_Discp> lista = new List<Dashboard.CountProf_Discp>();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = "SELECT * FROM CountProf_Discp";
+
+            try
+            {
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Dashboard.CountProf_Discp dc = new Dashboard.CountProf_Discp();
+                        dc.Nome = (string)reader["NomeGuerra"];
+                        dc.QtdDisciplinas = (int)reader["QtdDisciplinas"];
+
+                        lista.Add(dc);
+                    }
+                    return lista;
+                }
+            }
+            catch
+            {
+                return lista = null;
+            }
+        }
+
+        public List<Dashboard.CountProf_Curso> CountProf_Curso() //retorna qtd de cursos por professor
+        {
+            List<Dashboard.CountProf_Curso> lista = new List<Dashboard.CountProf_Curso>();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = "SELECT * FROM CountProf_Curso";
+
+            try
+            {
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Dashboard.CountProf_Curso dc = new Dashboard.CountProf_Curso();
+                        dc.Nome = (string)reader["NomeGuerra"];
+                        dc.QtdCursos = (int)reader["QtdCursos"];
+
+                        lista.Add(dc);
+                    }
+                    return lista;
+                }
+            }
+            catch
+            {
+                return lista = null;
+            }
+        }
+
+        public List<Dashboard.CountCurso_Discp> CountCurso_Discp() //retorna qtd de cursos por professor
+        {
+            List<Dashboard.CountCurso_Discp> lista = new List<Dashboard.CountCurso_Discp>();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = "SELECT * FROM CountCurso_Discp";
+
+            try
+            {
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Dashboard.CountCurso_Discp dc = new Dashboard.CountCurso_Discp();
+                        dc.Titulo = (string)reader["Titulo"];
+                        dc.Turno = (string)reader["Turno"];
+                        dc.QtdDisciplinas = (int)reader["QtdDisciplinas"];
+
+                        lista.Add(dc);
+                    }
+                    return lista;
+                }
+            }
+            catch
+            {
+                return lista = null;
+            }
+        }
+    }
+}
